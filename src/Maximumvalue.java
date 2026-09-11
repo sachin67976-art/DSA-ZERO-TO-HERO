400c1b8b343824e834cb25b2e79356e7de48e3a9
sachin67976-art <sachin67976@gmail.com>
2026-09-11 23:57:43 +0530

    Largestelement

--- src/Maximumvalue.java
+++ src/Maximumvalue.java
@@ -1,14 +1,15 @@
 public class Maximumvalue {
-    public static void main() {
+    public static void main(String[]args) {
 
 
-        int[] arr = {1, 2, 3, 45};
+        int[] num = {1, 100,2, 5};
         int ans = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > ans) {
-                ans = arr[i];
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] > ans) {
+                ans = num[i];
             }
-            System.out.println("max" + ans);
+        }
+            System.out.println("max= " + ans);
+
         }
     }
-}
