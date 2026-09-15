e9c1c12eb9654ebd72928249013a1028705b5924
sachin67976-art <sachin67976@gmail.com>
2026-09-16 00:35:26 +0530

    multidimensionalarray

---
+++ src/MultiDimensionalArray.java
@@ -0,0 +1,25 @@
+public class MultiDimensionalArray {
+
+    static void printarray(int[][] arr) {
+
+        for (int i = 0; i < arr.length; i++) { // row
+
+            for (int j = 0; j < arr[i].length; j++) { // column
+                System.out.print(arr[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] arr2 = {
+                {1, 5, 6},
+                {7, 9, 11},
+                {8, 1, 1}
+        };
+
+        printarray(arr2);
+    }
+}
