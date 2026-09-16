d193a70a9136886cef81a74a3009274af4cec3ee
sachin67976-art <sachin67976@gmail.com>
2026-09-16 22:02:51 +0530

    scanner multi dimensional array

--- src/MultiDimensionalArray.java
+++ src/MultiDimensionalArray.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class MultiDimensionalArray {
 
     static void printarray(int[][] arr) {
@@ -13,6 +14,7 @@ public class MultiDimensionalArray {
     }
 
     public static void main(String[] args) {
+
 
         int[][] arr2 = {
                 {1, 5, 6},
